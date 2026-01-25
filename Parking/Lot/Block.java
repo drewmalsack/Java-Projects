651328fdbca0e33adb2556fbfc2a55d44184e4dd
Andrew Malsack <drewmalsack@gmail.com>
2026-01-25 06:52:47 -0600

    Added boiler plate code and methods for Space and Block classes.

--- Parking/Lot/Block.java
+++ Parking/Lot/Block.java
@@ -2,7 +2,19 @@ package Parking.Lot;
 
 public class Block {
     
+    //holds the space using this block, if there is one.
+    private Space space;
+
     public Block(){
 
     }
+
+    public void setSpace(Space space){
+        this.space = space;
+    }
+
+    public Space getSpace(){
+        return this.space;
+    }
+
 }
--- Parking/Lot/Space.java
+++ Parking/Lot/Space.java
@@ -1,7 +1,21 @@
 package Parking.Lot;
 
+import java.util.List;
+
 public class Space {
-    public Space() {
-        
+
+    //List of blocks(physical space) that the space uses
+    private List<Block> blocks;
+
+    public Space(List<Block> blocks) {
+        this.blocks = blocks;
+    }
+
+    public void setBlocks(List<Block> blocks){
+        this.blocks = blocks;
+    }
+
+    public List<Block> getBlocks(){
+        return this.blocks;
     }
 }
