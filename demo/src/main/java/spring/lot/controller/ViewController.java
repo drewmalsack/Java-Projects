4cf27b11ad710adcc338bbec08d74577a44cd2c3
Andrew Malsack <drewmalsack@gmail.com>
2026-02-06 06:59:42 -0600

    Added thymeleaf starter to projects pom.xml
    Created a ViewController to handle serving thymeleaf html code
    
    Created lot-view.html to show a grid view of the current lot setup
    
    Added viewLot method in ViewController to serve the lot-view.html
    
    Added processForm method in ViewController to handle form logic on the backend.

---
+++ demo/src/main/java/spring/lot/controller/ViewController.java
@@ -0,0 +1,35 @@
+package spring.lot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import spring.lot.model.Space;
+import spring.lot.service.LotService;
+
+@Controller
+public class ViewController {
+    
+    @Autowired
+    private LotService lotService;
+
+    @GetMapping("/lot-view")
+    public String viewLot(Model model){
+
+        model.addAttribute("lot", lotService.getLot());
+        return "lot-view";
+    }
+
+    @PostMapping("/lot-view")
+    public String processForm(RedirectAttributes redirectAttributes, @RequestParam int x_coord, @RequestParam int y_coord, @RequestParam int length, @RequestParam int width){
+
+        Space newSpace = lotService.addSpace(length, width, x_coord, y_coord);
+        if(newSpace == null)
+            redirectAttributes.addFlashAttribute("errorMessage", "Could not create space. Check for overlaps or boundaries.");
+        return "redirect:/lot-view";
+    }
+}
