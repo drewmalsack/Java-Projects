cafc91af9400cc70935d10725780066bd49fdc49
Andrew Malsack <drewmalsack@gmail.com>
2026-02-08 07:26:43 -0600

    Added transactional annotation to the LotService class

--- demo/src/main/java/spring/lot/service/LotService.java
+++ demo/src/main/java/spring/lot/service/LotService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import spring.lot.model.Block;
 import spring.lot.model.Lot;
@@ -13,6 +14,7 @@ import spring.lot.repository.LotRepository;
 import spring.lot.repository.SpaceRepository;
 
 @Service
+@Transactional
 public class LotService {
 
     //public final Lot lot = new Lot(10, 10);
