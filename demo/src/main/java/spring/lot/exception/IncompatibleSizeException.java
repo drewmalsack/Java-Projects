d24786cb9ecbf07086dfd230310c16731df43a2b
Andrew Malsack <drewmalsack@gmail.com>
2026-02-12 02:39:56 -0600

    Created IncompatibleSize and SpaceOccupied exceptions to be used in vehicle parking logic.

---
+++ demo/src/main/java/spring/lot/exception/IncompatibleSizeException.java
@@ -0,0 +1,7 @@
+package spring.lot.exception;
+
+public class IncompatibleSizeException extends ParkingException{
+
+    public IncompatibleSizeException(String message) { super(message);}
+
+}
---
+++ demo/src/main/java/spring/lot/exception/SpaceOccupiedException.java
@@ -0,0 +1,8 @@
+package spring.lot.exception;
+
+public class SpaceOccupiedException extends ParkingException {
+
+    public SpaceOccupiedException(String message) { super(message);}
+
+}
+
